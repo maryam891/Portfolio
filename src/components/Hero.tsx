3863ce32924b6bb75dc1ac5791e13dab189e491e
Maryam <maryam.chaudhry@iths.se>
2026-04-28 11:02:20 +0200

    changeing text inte hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
           I have built various applications React, JavaScript och Vue, that you can see in my projects.
         </p>
         <div className={"hero-links-container"}>
-          <a href="#Projects" className={"see-project-link"}>View projects on Github →</a>
+          <a href="#Projects" className={"see-project-link"}>View project →</a>
           <a href="#Contact" className={"contact-link"}>Contact me</a>
         </div>
       </div>
