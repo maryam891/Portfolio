85d3b32bbca951f0153de55dca345c1f41da3637
Maryam <maryam.chaudhry@iths.se>
2026-05-01 19:52:22 +0200

    adding changes to about text and small changes to text in hero section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ const skills = [
   { label: 'Strong in', items: 'React · TypeScript · JavaScript · HTML & CSS · UX/UI Design · Graphic design' },
   { label: 'Intermediate in', items: 'Node.js · SQL/SQLite · Vue.js · WordPress · Express' },
   { label: 'Tools', items: 'Git · Jira · React bootstrap · Vitest · Jest' },
-  { label: 'Language', items: 'Svenska · Engelska · Urdu' },
+  { label: 'Language', items: 'Swedish · English · Urdu' },
 ]
 
 export default function About() {
@@ -15,9 +15,9 @@ export default function About() {
       <div className={"about-label"}><h2 >About me</h2></div>
       <div className={"main-about-container"} >
         <div className={"about-text-container"}>
-          <p>I am currently studying my last term at IT-Högskolan as a Frontend Developer, with a background as UX & UI design. Those combinations makes it so that I think both through the users perspective and about code and see it as a whole.</p>
-          <p>Right now I am doing my second internship at Viscando AB where I also had my first internship. At Viscando I build a visual user interface with React, TypeScript, Json Forms and Reaxct Flow. I thrive in a structured team where we can share ideas and learn from each other.</p>
-          <p>I am looking for my first job after my graduation in june of 2026.</p>
+          <p>I am currently studying my last term at IT-Högskolan as a Frontend Developer, with a background in UX & UI design. That combinations means I that I think both through the user's perspective while also thinking about code, seeing the two as a whole.</p>
+          <p>Right now I am doing my second internship at Viscando AB, where I also had my first internship. At Viscando I am building a visual user interface with React, TypeScript, JSON Forms and React Flow. I thrive in a structured team where we can share ideas and learn from each other.</p>
+          <p>I am looking for my first job after my graduation in June 2026.</p>
         </div>
         <div className={"skills-container"}>
           {skills.map((s) => (
