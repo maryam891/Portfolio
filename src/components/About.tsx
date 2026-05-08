51ad0e21d8c2c3be0991c3c32b0a01162c4e2b2e
Maryam <maryam.chaudhry@iths.se>
2026-05-09 00:25:08 +0200

    added small change to skills

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@
 import '../css/About.css'
 
 const skills = [
-  { label: 'Programming Language & Frameworks', items: 'React · TypeScript · JavaScript · HTML & CSS · UX/UI Design · Graphic design,Node.js · SQL/SQLite · Vue.js · WordPress · Express' },
+  { label: 'Programming Language & Frameworks', items: 'React · TypeScript · JavaScript · HTML & CSS · UX/UI Design · Graphic design · Node.js · SQL/SQLite · Vue.js · WordPress · Express' },
   { label: 'Tools', items: 'Git · Jira · React bootstrap · Vitest · Jest, Figma' },
   { label: 'Language', items: 'Swedish · English · Urdu' },
 ]
