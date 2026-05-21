4245d84b3f9d16a50f3404a72d6a7b489a3595c8
Maryam <maryam.chaudhry@iths.se>
2026-05-21 22:06:12 +0200

    added small change in text for footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import '../css/Footer.css'
 export default function Footer() {
   return (
     <footer className={"footer"}>
-      <p>Maryam Chaudhry · Frontend Developer · Gotheburg</p>
+      <p>Maryam Chaudhry · Frontend Developer · Gothenburg</p>
     </footer>
   )
 }
