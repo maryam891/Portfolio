e73a0ee9cecfe411e914e6b9d29aab21870911b1
Maryam <maryam.chaudhry@iths.se>
2026-06-08 15:29:01 +0200

    added updates to second internship text in about section and experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,13 +13,13 @@ export default function Experience() {
             creative and problem-solving aspects of it.
           </p>
           <p>
-            I am currently doing my second internship at Viscando AB. During my
+            I recently completed my second internship at Viscando AB. During my
             first internship I built an application with React and TypeScript for
             visualization of VPN/locally connected systems, and wrote unit tests
             with Jest for the backend and Vitest for the frontend.
           </p>
           <p>
-            During my second internship, I am working in a team to build a user interface for a camera system using React, TypeScript, Node.js, React Flow and JSON Forms. I also present demos during development meetings to gather feedback and improve the user interface.
+            During my second internship, I worked in a team with another team mate to build a user interface for a camera system using React, TypeScript, Node.js, React Flow and JSON Forms. I also presented demos during development meetings to gather feedback and improve the user interface.
           </p>
           <p>
             I previously studied UX & UI Design, which taught me to think from
