558ae3e50ccf933546cbe39e6cd23442635fa861
Maryam <maryam.chaudhry@iths.se>
2026-06-16 10:43:39 +0200

    updated text in experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -13,12 +13,12 @@ export default function Experience() {
             creative and problem-solving aspects of it.
           </p>
           <p>
-            I recently graduated from IT-Hogskolan and completed my second internship at Viscando AB. During my first internship I built an application with React and TypeScript for
+            I recently graduated from IT-Hogskolan and completed my second internship at Viscando AB. During my first internship I built a tool with React and TypeScript for
             visualization of VPN/locally connected systems, and wrote unit tests
             with Jest for the backend and Vitest for the frontend.
           </p>
           <p>
-            During my second internship, I worked in a team with another team mate to build a user interface for a camera system using React, TypeScript, Node.js, React Flow and JSON Forms. I also presented demos during development meetings to gather feedback and improve the user interface. Additionally I worked on my thesis at Viscando AB where I got to further develop the interface with new pages such as an overview page with new pages including an overview of camera sensors with graph data and diagrams.
+            During my second internship, I worked in a agile team to build a user interface for a camera system using React, TypeScript, Node.js, React Flow and JSON Forms. I also presented demos during development meetings to gather feedback and improve the user interface. Additionally I worked on my thesis at Viscando AB where I got to further develop the interface with new pages such as an overview page with new pages including an overview of camera sensors with graph data and diagrams.
           </p>
           <p>
             I previously studied UX & UI Design, which taught me to think from
